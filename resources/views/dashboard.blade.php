b08ab4236660dcceb05a29b4a42faa9eac4c7b5a
Anas Anwer <anas.anwer@pegasync.com>
2024-03-01 01:29:28 +0500

    changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -94,7 +94,7 @@
                 processing: true,
                 serverSide: true,
                 ajax: "{{ route('profile_list') }}",
-                autoWidth: true,
+                autoWidth: false,
                 lengthMenu: [[5], ['5']],
                 columns: [
                     {data: 'DT_RowIndex', orderable: false, searchable: false},
