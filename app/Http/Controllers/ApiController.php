84bfb8585c7d8a9339be13e69e715c034a6876ae
Muhammad Shariq <m.shariq1993@gmail.com>
2024-03-04 16:44:54 +0500

    changes

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -18,7 +18,8 @@ class ApiController extends Controller
             'phone_number' => 'nullable',
             'email_address' => 'nullable|email',
             'linkedin_url' => 'nullable|url',
-            'picture' => 'nullable|image'
+            'picture' => 'nullable|image',
+            'user_id' => 'nullable|int',
         ];
 
         $validate = Validator::make($request->all(), $rules);
@@ -26,13 +27,22 @@ class ApiController extends Controller
         if ($validate->fails()) {
             return response()->json(['status' => 0, 'message' => 'Error(s) in Input', 'errors' => $validate->errors()]);
         } else {
-            if ($request->name != null || $request->phone_number != null || $request->email_address != null || $request->linkedin_url != null || $request->picture != null) {
 
+            $user_id = 1;
+
+            if($request->has('user_id') && $request->user_id != 1)
+            {
+                $user_id = $request->user_id;
+            }
+
+            if ($request->name != null || $request->phone_number != null || $request->email_address != null || $request->linkedin_url != null || $request->picture != null)
+            {
                 $profile = new Profile();
                 $profile->name = $request->name;
                 $profile->phone_number = $request->phone_number;
                 $profile->email_address = $request->email_address;
                 $profile->linkedin_url = $request->linkedin_url;
+                $profile->user_id = $user_id;
                 $profile->save();
 
                 if ($request->has('picture') && $request->picture != null) {
@@ -45,21 +55,21 @@ class ApiController extends Controller
                     $profile->save();
                 }
 
-		 if ($request->has('email_address') && $request->email_address != null) {
-                    // Mail::to($request->email_address)->send(new SendEmailNotification());
+                if (($request->has('email_address') && $request->email_address != null) && $user_id == 1)
+                {
                     Mail::to($request->email_address)->send(new NewRecordEmail($request->name));
                 }
 
                 return response()->json(['status' => 1, 'message' => 'Profile created successfully'], 200);
-            } else {
+            }
+            else {
                 return response()->json(['status' => 1, 'message' => 'Invalid Data'], 200);
             }
         }
     }
 
-    public function profile_list(){
-        $profiles = Profile::get();
-
+    public function profile_list($user_id){
+        $profiles = Profile::where('user_id',$user_id)->get();
         return response()->json(['status' => 1, 'profiles' => $profiles]);
     }
 }
